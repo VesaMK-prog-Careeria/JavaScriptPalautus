0fa95e9f21986b7b9cc146066736c86bbefb11b2
Vesa Kalliomäki <vesa.kalliomaki@student.careeria.fi>
2024-02-11 14:03:43 +0200

    siistiminen

--- node.js
+++ node.js
@@ -1,43 +1,44 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const http = require('http');
-const { Server } = require('socket.io');
+const express = require('express'); // Express on Node.js:n web-sovelluskehys
+const fs = require('fs'); // Moduuli tiedostojen lukemiseen ja kirjoittamiseen
+const path = require('path'); // Moduuli polkujen käsittelyyn
+const http = require('http'); // Moduuli HTTP-palvelimen luomiseen
+const { Server } = require('socket.io');  // Moduuli reaaliaikaiseen viestintään
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
-const port = process.env.PORT || 3000;
+const app = express(); // Luo uusi Express-sovellus
+const server = http.createServer(app); // Luo uusi HTTP-palvelin
+const io = new Server(server); // Luo uusi Socket.IO -palvelin
+const port = process.env.PORT || 3000; // Portti, jota käytetään
+
 
 // Tarjoile staattiset tiedostot (esim. CSS, JavaScript)
 app.use(express.static('public'));
 
-app.get('/henkilosto.json', (req, res) => {
-  fs.readFile('henkilosto.json', (err, data) => {
-    if (err) throw err;
-    res.contentType('application/json');
-    res.send(data);
+app.get('/henkilosto.json', (req, res) => { // Lähetä henkilöstötiedot JSON-muodossa
+  fs.readFile('henkilosto.json', (err, data) => { // Lue tiedosto
+    if (err) throw err; // Jos virhe, heitä poikkeus
+    res.contentType('application/json'); // Aseta oikea Content-Type
+    res.send(data); // Lähetä tiedot
   });
 });
 
 // Reaaliaikainen chat-toiminnallisuus
-io.on('connection', (socket) => {
-  console.log('Käyttäjä yhdistetty');
+io.on('connection', (socket) => { // Kun käyttäjä yhdistää
+  console.log('Käyttäjä yhdistetty'); // Kirjoita konsoliin, että käyttäjä on yhdistänyt
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+  socket.on('chat message', (msg) => { // Kun palvelin saa viestin
+    io.emit('chat message', msg); // Lähetä viesti kaikille käyttäjille
   });
 
-  socket.on('disconnect', () => {
+  socket.on('disconnect', () => { 
     console.log('Käyttäjä katkaissut yhteyden');
   });
 });
 
 // Lähetä yksi HTML-sivu kaikille reiteille, paitsi jos määritelty toisin
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get('*', (req, res) => { // Tähti (*) tarkoittaa kaikkia reittejä
+  res.sendFile(path.join(__dirname, 'public', 'index.html')); // Lähetä HTML-sivu
 });
 
-server.listen(port, () => {
-  console.log(`Palvelin käynnissä portissa ${port}`);
+server.listen(port, () => { // Käynnistä palvelin
+  console.log(`Palvelin käynnissä portissa ${port}`); // Kirjoita konsoliin, että palvelin on käynnistynyt
 });
--- public/script.js
+++ public/script.js
@@ -1,11 +1,13 @@
 
-$(document).ready(function() {
-    $('#darktheme').click(function() {
-        $('body').toggleClass('dark-mode');
+$(document).ready(function() { // Kun sivu on ladattu, suorita tämä
+    $('#darktheme').click(function() { // Kun darktheme-nappia painetaan, suorita tämä
+        $('body').toggleClass('dark-mode'); 
         $('table').toggleClass('dark-mode');
         $('th').toggleClass('dark-mode');
         $('td').toggleClass('dark-mode');
         $('h2').toggleClass('dark-mode');
+        $('chat').toggleClass('dark-mode');
+
     });
     $('#etusivu').click(function() {
         $('#main_alue').html('Tervetuloa yrityksemme kotisivulle!');
@@ -16,27 +18,28 @@ $(document).ready(function() {
     });
 
     $('#yhteystiedot').click(function() {
-        $('#main_alue').html('<h2>Yhteystiedot</h2><p>Voit löytää meidät...</p>');
+        $('#main_alue').html('<h2>Yhteystiedot</h2><p>Löydät meidät täältä</p><p>Puhelin: 123456789</p><p>Sähköposti:maija.meikalainen@yritys.fi</p>');
+        $('#main_alue').append('<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3213.398889116665!2d26.941075152854186!3d60.46690718051227!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sfi!2sfi!4v1707650585022!5m2!1sfi!2sfi" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>');
     });
 
-    $('#henkilokunta').click(function() {
-        $.getJSON('/henkilosto.json', function(data) {
-            var henkilosto = '<h2>Henkilökunta</h2><table><tr><th>Nimi</th><th>Tehtävä</th></tr>';
-            $.each(data, function(key, val) {
-                henkilosto += '<tr><td>' + val.nimi + '</td><td>' + val.tehtava + '</td></tr>';
+    $('#henkilokunta').click(function() { 
+        $.getJSON('/henkilosto.json', function(data) { // Hae henkilöstötiedot JSON-tiedostosta
+            var henkilosto = '<h2>Henkilökunta</h2><table><tr><th>Nimi</th><th>Tehtävä</th></tr>'; // Luo taulukko
+            $.each(data, function(key, val) { // Käy JSON-tiedosto läpi
+                henkilosto += '<tr><td>' + val.nimi + '</td><td>' + val.tehtava + '</td></tr>'; // Lisää taulukkoon henkilön nimi ja tehtävä
             });
-            henkilosto += '</table>';
-            $('#main_alue').html(henkilosto);
+            henkilosto += '</table>'; // Sulje taulukko
+            $('#main_alue').html(henkilosto); // Lisää taulukko sivulle
         });
     });
 });
-$(function () {
-    var socket = io();
+$(function () { 
+    var socket = io(); // Luo yhteys palvelimeen
     var user = ''; // Nimimerkkiä varten
 
     // Kysy nimimerkkiä vain kerran, kun chat avataan ensimmäistä kertaa
     $('#avaaChat').one('click', function() {
-        user = window.prompt('Anna nimimerkki:');
+        user = window.prompt('Anna nimimerkki:'); 
         if (user) { // Lähetä liittymisviesti vain, jos käyttäjänimi on annettu
             socket.emit('chat message', user + " liittyi keskusteluun.");
         }
@@ -52,16 +55,16 @@ $(function () {
     $('form').submit(function(e) {
         e.preventDefault(); // Estää lomakkeen lähetyksen
         // Tarkista onko käyttäjänimi asetettu ennen viestin lähetystä
-        if (user && $('#input').val().trim() !== '') {
-            socket.emit('chat message', user + ' kirjoitti: ' + $('#input').val());
-            $('#input').val('');
+        if (user && $('#input').val().trim() !== '') { // Lähetä viesti vain, jos käyttäjänimi on annettu ja viestissä on tekstiä
+            socket.emit('chat message', user + ' kirjoitti: ' + $('#input').val()); // Lähetä viesti palvelimelle
+            $('#input').val(''); // Tyhjennä tekstikenttä
         }
-        return false;
+        return false; 
     });
 
-    socket.on('chat message', function(msg) {
-        $('#messages').append($('<li>').text(msg));
-        window.scrollTo(0, document.body.scrollHeight);
+    socket.on('chat message', function(msg) { // Kun palvelin lähettää viestin
+        $('#messages').append($('<li>').text(msg)); // Lisää viesti sivulle
+        window.scrollTo(0, document.body.scrollHeight); // Vieritä sivu alaspäin, jotta uusin viesti näkyy
     });
 });
 
